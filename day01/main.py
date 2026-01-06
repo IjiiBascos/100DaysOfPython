2a646cde4320c6872e9f8d8d9a430997f8251255
Yopfiel Bascos <bscs.yop@gmail.com>
2026-01-06 19:24:30 +0800

    first commit

---
+++ day01/main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+city = input("What city did you grow up in?: ")
+pet = input("What is the name of your first pet?: ")
+
+print(f"Your band name is {city} {pet}")
